122d5668cb07f8c160e414de56338afb2e030297
Your Name <muresanvladmihail@gmail.com>
2016-06-19 20:29:08 +0300

    rectangle works

--- src/graphics/geometry.c
+++ src/graphics/geometry.c
@@ -91,7 +91,6 @@ static void drawBuffer(int vertices, int indices, GLenum type) {
   graphics_setShader(shader);
 }
 
-//This piece of shit does not want to work
 void graphics_geometry_drawCircle(float x, float y, float radius, int segments) {
   growBuffers(segments+1,segments+2);
   
@@ -159,33 +158,45 @@ void graphics_geometry_fillRectangle(int type, float x, float y, float w, float 
   
   float *buf = moduleData.data;
 
-  buf[0] = 1.0f; //0
-  buf[1] = 0.0f;
-    
+  buf[0] = x + w * sx; //0
+  buf[1] = y;
+  
+  buf[2] = 0.0f;
+  buf[3] = 0.0f;
+
   buf[4] = 1.0f; 
   buf[5] = 1.0f;
   buf[6] = 1.0f;
   buf[7] = 1.0f;
 
-  buf[8] = 0.0f; //1
-  buf[9] = 0.0f;
+  buf[8] = x; //1
+  buf[9] = y;
   
+  buf[10] = 0.0f;
+  buf[11] = 0.0f;
+
   buf[12] = 1.0f;
   buf[13] = 1.0f;
   buf[14] = 1.0f;
   buf[15] = 1.0f;
 
-  buf[16] = 0.0f; //2
-  buf[17] = 1.0f;
-    
+  buf[16] = x; //2
+  buf[17] = y + h * sy;
+  
+  buf[18] = 0.0f;
+  buf[19] = 0.0f;  
+
   buf[20] = 1.0f;
   buf[21] = 1.0f;
   buf[22] = 1.0f;
   buf[23] = 1.0f;
 
-  buf[24] = 1.0f; //3
-  buf[25] = 1.0f;
-    
+  buf[24] = x + w * sx; //3
+  buf[25] = y + h * sy;
+  
+  buf[26] = 0.0f;
+  buf[27] = 0.0f;
+
   buf[28] = 1.0f;
   buf[29] = 1.0f;
   buf[30] = 1.0f;
@@ -195,47 +206,19 @@ void graphics_geometry_fillRectangle(int type, float x, float y, float w, float 
   moduleData.index[1] = 1;
   moduleData.index[2] = 3;
   moduleData.index[3] = 2;
-  /*
+  
   if (type == 1) 
-    drawBuffer(x,y,w,h,rotation,sx,sy,ox,oy, 32, 5, GL_TRIANGLE_STRIP);
+    drawBuffer(32, 4, GL_TRIANGLE_STRIP);
   else 
-    drawBuffer(x,y,w,h,rotation,sx,sy,ox,oy, 32, 5, GL_LINE_STRIP);
-	*/
-	 }
+    drawBuffer(32, 4, GL_LINE_STRIP);
+	
+	}
 
 void graphics_geometry_points(float x, float y) {
-  //growBuffers(2,2);
-  //drawBuffer(x,y,1,1,0,1,1,0,0,2,2,GL_POINTS);
 }
 
-//TODO not 100% functional 
 void graphics_geometry_vertex(float x, float y, int vertices[], int count) {
-  growBuffers(count*8, count*2);
   
-  float *buf = moduleData.data;
-     
-  if (count != 1) {   
-     for (int i = 0; i < count*2; i++) { 
-         //printf("%d %d \n", vertices[i], vertices[i+1]);
-         buf[0] = vertices[i]; 
-         buf[1] = vertices[i+1]; 
-     
-         buf[2] = 0.0f;
-         buf[3] = 0.0f;
-
-         buf[4] = 1.0f;
-         buf[5] = 1.0f;
-         buf[6] = 1.0f;
-         buf[7] = 1.0f; 
-     }
-   }
-    
-  for(int i = 0; i < count; i++) {
-    //printf("%d \n",i);
-    moduleData.index[i] = i;
-  }
-
-  //drawBuffer(x, y, 50, 50, 0, 1, 1, 0, 0, count*8, count*2, GL_LINE_LOOP);
 }
 
 
