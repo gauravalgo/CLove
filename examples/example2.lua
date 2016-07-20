ec216efb06df7c506937c4e76b511f70ee249560
Your Name <muresanvladmihail@gmail.com>
2016-07-20 20:08:05 +0300

    update example so you know how to use setPixel from newImageData

--- examples/example2.lua
+++ examples/example2.lua
@@ -6,14 +6,14 @@
 --
 --]]
 
-
 local t = {}
 function spawn(x,y)
 	table.insert(t,{x=x,y=y})
 end
-
+local dr = nil
 function love.load()
 		
+	--read the pixels color and do stuff based on that
 	local im = love.image.newImageData("map.png")
 	
 	for y = 1, im:getHeight() do
@@ -26,7 +26,20 @@ function love.load()
         end
       end
 	end
-
+	
+	-- set the pixels to a custom color
+	local data = love.image.newImageData(132,132)
+	for i=1,131 do   
+		for k=1,131 do 
+   		data:setPixel(i, k, math.random(1,155), math.random(1,155), math.random(1,255), 255)
+		end
+	end
+	
+	
+	--load the new made texture with custom colors and size and draw it in
+	--love.draw
+	dr = love.graphics.newImage(data)
+	dr:setFilter("nearest","nearest")
 end
 
 function love.update(dt)
@@ -43,9 +56,9 @@ end
 function love.draw()
 	
 	for i,v in ipairs(t) do 
-		love.graphics.rectangle("fill",v.x*16,v.y*16,16,16)
+		--love.graphics.rectangle("fill",v.x*16,v.y*16,16,16)
 	end
-	
+	love.graphics.draw(dr,200,200)
 	love.graphics.print(""..love.timer.getFPS(),10,10)
 end
 
